2c3cb5cb1de3e63aa6af8cab9eec6981f9d35efd
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:28:39 +0300

    AboutController hazırlandı.

---
+++ Presentation/Areas/Management/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract.Base;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using Presentation.Areas.Management.Controllers.Base;
+using Presentation.Extensions;
+using System.Threading.Tasks;
+
+namespace Presentation.Areas.Management.Controllers;
+
+public class AboutController : BaseController
+{
+    public AboutController(IServiceManager serviceManager, IToastNotification toastNotification) : base(serviceManager, toastNotification)
+    {
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var result = await _serviceManager.AboutService.GetAboutAsync();
+        return this.ResponseView(result);
+    }
+}
