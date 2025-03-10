432d6b7f174e6748e3b21eec3882fd47ff41b5fe
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:23:06 +0300

    SocialMedia Servis yapısı hazırlandı.

---
+++ Business/Concrete/SocialMediaManager.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Business.Abstract;
+using Business.Concrete.Base;
+using Business.Helpers.Validations;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.Dtos;
+using Portfolio.Core.Utilities.Results.Abstract;
+using Portfolio.Core.Utilities.Results.ComplexTypes;
+using Portfolio.Core.Utilities.Results.Concrete;
+
+namespace Business.Concrete;
+
+public class SocialMediaManager : BaseManager, ISocialMediaService
+{
+    public SocialMediaManager(IValidationHelper validationHelper, IRepository repository, IMapper mapper) : base(validationHelper, repository, mapper)
+    {
+    }
+
+    public async Task<IDataResult<IList<GetAllSocialMediaIconDto>>> GetAllAsync()
+    {
+        IList<SocialMediaIcon> socialMedias = await Repository.GetRepository<SocialMediaIcon>().GetAllAsync();
+        List<GetAllSocialMediaIconDto> getAllSocialMediaDtos = Mapper.Map<List<GetAllSocialMediaIconDto>>(socialMedias);
+        return new DataResult<List<GetAllSocialMediaIconDto>>(ResultStatus.Success, getAllSocialMediaDtos);
+    }
+}
