b375f64eb0f0a0aeea834fb852736971ef427cf7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:29:14 +0300

    CertificateController hazırlandı.

---
+++ Presentation/Areas/Management/Controllers/CertificateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Areas.Management.Controllers;
+
+[Area("Management")]
+public class CertificateController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
