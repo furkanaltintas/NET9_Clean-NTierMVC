22d91a97a79fa4bb7abc362d27b7afefdf85e9c8
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:31:51 +0300

    BaseController hazırlandı.

---
+++ Presentation/Areas/Management/Controllers/Base/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Business.Abstract.Base;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+
+namespace Presentation.Areas.Management.Controllers.Base;
+
+[Area("Management")]
+[Route("management/[controller]")]
+public class BaseController : Controller
+{
+    protected readonly IServiceManager _serviceManager;
+    protected readonly IToastNotification _toastNotification;
+
+    public BaseController(IServiceManager serviceManager, IToastNotification toastNotification)
+    {
+        _serviceManager = serviceManager;
+        _toastNotification = toastNotification;
+    }
+}
