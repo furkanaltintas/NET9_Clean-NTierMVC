6d973bb9b15ee82b55bdf7314b1474645a1ed976
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:31:06 +0300

    SkillController hazırlandı.

---
+++ Presentation/Areas/Management/Controllers/SkillController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract.Base;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using Presentation.Areas.Management.Controllers.Base;
+using Presentation.Areas.Management.ViewModels;
+using Presentation.Extensions;
+
+namespace Presentation.Areas.Management.Controllers;
+
+public class SkillController : BaseController
+{
+    public SkillController(IServiceManager serviceManager, IToastNotification toastNotification) : base(serviceManager, toastNotification)
+    {
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var result = await _serviceManager.SkillService.GetAllAsync();
+        return this.ResponseViewModel<GetAllSkillDto, SkillViewModel>(result);
+    }
+}
---
+++ Presentation/Areas/Management/ViewModels/SkillViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Abstract;
+using Entities.Dtos;
+
+namespace Presentation.Areas.Management.ViewModels;
+
+public class SkillViewModel : IViewModel
+{
+    public SkillViewModel(GetAllSkillDto getAllSkillDto)
+    {
+        Id = getAllSkillDto.Id;
+        Name = getAllSkillDto.Name;
+        Point = getAllSkillDto.Point;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Point { get; set; }
+}
