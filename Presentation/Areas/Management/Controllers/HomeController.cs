306bc8880b73739654d67537acb52a8e46a3c356
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:30:10 +0300

    HomeController hazırlandı.

---
+++ Presentation/Areas/Management/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Areas.Management.Controllers;
+
+[Area("Management")]
+[Route("management/[controller]")]
+public class HomeController : Controller
+{
+    public IActionResult Index() => View();
+}
