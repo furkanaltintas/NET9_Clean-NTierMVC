d1314df5d49abc5ae740bca00672a8d6918e176c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:29:35 +0300

    EducationController hazırlandı.

---
+++ Presentation/Areas/Management/Controllers/EducationController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract.Base;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using Presentation.Areas.Management.Controllers.Base;
+using Presentation.Areas.Management.ViewModels;
+using Presentation.Extensions;
+
+namespace Presentation.Areas.Management.Controllers;
+
+public class EducationController : BaseController
+{
+    public EducationController(IServiceManager serviceManager, IToastNotification toastNotification) : base(serviceManager, toastNotification)
+    {
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var result = await _serviceManager.EducationService.GetAllAsync();
+        return this.ResponseViewModel<GetAllEducationDto, EducationViewModel>(result);
+    }
+}
---
+++ Presentation/Areas/Management/ViewModels/EducationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Abstract;
+using Entities.Dtos;
+
+namespace Presentation.Areas.Management.ViewModels;
+
+public class EducationViewModel : IViewModel
+{
+    public EducationViewModel(GetAllEducationDto getAllEducationDto)
+    {
+        Id = getAllEducationDto.Id;
+        Title = getAllEducationDto.Title;
+        Description = getAllEducationDto.Description;
+        StartDate = getAllEducationDto.StartDate.ToString("dd/MM/yyyy");
+        EndDate = getAllEducationDto.EndDate.ToString("dd/MM/yyyy");
+    }
+
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string StartDate { get; set; }
+    public string EndDate { get; set; }
+}
