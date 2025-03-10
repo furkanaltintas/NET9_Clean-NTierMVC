f158bc7de9c59f7c5e48233cd02489cda5b8c45f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:30:29 +0300

    PortfolioController hazırlandı.

---
+++ Presentation/Areas/Management/Controllers/PortfolioController.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract.Base;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using Presentation.Areas.Management.Controllers.Base;
+using Presentation.Areas.Management.ViewModels;
+using Presentation.Extensions;
+using System.Threading.Tasks;
+
+namespace Presentation.Areas.Management.Controllers;
+
+[Area("Management")]
+[Route("yonetim/[controller]")]
+public class PortfolioController : BaseController
+{
+    public PortfolioController(IServiceManager serviceManager, IToastNotification toastNotification) : base(serviceManager, toastNotification) { }
+
+    public async Task<IActionResult> Index()
+    {
+        var result = await _serviceManager.PortfolioService.GetAllAsync();
+        return this.ResponseViewModel<GetAllPortfolioDto, PortfolioViewModel>(result);
+    }
+}
---
+++ Presentation/Areas/Management/ViewModels/PortfolioViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Abstract;
+using Entities.Dtos;
+
+namespace Presentation.Areas.Management.ViewModels;
+
+public class PortfolioViewModel : IViewModel
+{
+    public PortfolioViewModel(GetAllPortfolioDto getAllPortfolioDto)
+    {
+        Id = getAllPortfolioDto.Id;
+        Title = getAllPortfolioDto.Title;
+        SubTitle = getAllPortfolioDto.SubTitle;
+        Image = getAllPortfolioDto.Image;
+    }
+
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string SubTitle { get; set; }
+    public string Image { get; set; }
+}
