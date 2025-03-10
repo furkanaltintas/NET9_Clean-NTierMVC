9fdb7bf666dd4e18352cc07ac27ac552533db249
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:30:46 +0300

    ServiceController hazırlandı.

---
+++ Presentation/Areas/Management/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract.Base;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using Presentation.Areas.Management.Controllers.Base;
+using Presentation.Areas.Management.ViewModels;
+using Presentation.Extensions;
+
+namespace Presentation.Areas.Management.Controllers;
+
+public class ServiceController : BaseController
+{
+    public ServiceController(IServiceManager serviceManager, IToastNotification toastNotification) : base(serviceManager, toastNotification)
+    {
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var result = await _serviceManager.ServiceService.GetAllAsync();
+        return this.ResponseViewModel<GetAllServiceDto, ServiceViewModel>(result);
+    }
+}
---
+++ Presentation/Areas/Management/ViewModels/ServiceViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Abstract;
+using Entities.Dtos;
+
+namespace Presentation.Areas.Management.ViewModels;
+
+public class ServiceViewModel : IViewModel
+{
+    public ServiceViewModel(GetAllServiceDto getAllServiceDto)
+    {
+        Id = getAllServiceDto.Id;
+        Name = getAllServiceDto.Name;
+        Description = getAllServiceDto.Description;
+        Icon = getAllServiceDto.Icon;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Icon { get; set; }
+}
