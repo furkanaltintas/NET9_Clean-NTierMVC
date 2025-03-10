8934ec6eb993e85be6b00e25281871675e91bd88
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:29:52 +0300

    ExperienceController hazırlandı.

---
+++ Presentation/Areas/Management/Controllers/ExperienceController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract.Base;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using Presentation.Areas.Management.Controllers.Base;
+using Presentation.Areas.Management.ViewModels;
+using Presentation.Extensions;
+
+namespace Presentation.Areas.Management.Controllers;
+
+public class ExperienceController : BaseController
+{
+    public ExperienceController(IServiceManager serviceManager, IToastNotification toastNotification) : base(serviceManager, toastNotification)
+    {
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var result = await _serviceManager.ExperienceService.GetAllAsync();
+        return this.ResponseViewModel<GetAllExperienceDto, ExperienceViewModel>(result);
+    }
+}
---
+++ Presentation/Areas/Management/ViewModels/ExperienceViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities.Abstract;
+using Entities.Dtos;
+
+namespace Presentation.Areas.Management.ViewModels;
+
+public class ExperienceViewModel : IViewModel
+{
+    public ExperienceViewModel(GetAllExperienceDto getAllExperienceDto)
+    {
+        Id = getAllExperienceDto.Id;
+        Title = getAllExperienceDto.Title;
+        Company = getAllExperienceDto.Company;
+        Description = getAllExperienceDto.Description;
+        Location = getAllExperienceDto.Location;
+        TypeOfEmployment = getAllExperienceDto.TypeOfEmployment;
+        StartDate = getAllExperienceDto.StartDate.ToString("dd/MM/yyyy");
+        EndDate = getAllExperienceDto.EndDate.ToString("dd/MM/yyyy");
+    }
+
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Company { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public string TypeOfEmployment { get; set; }
+    public string StartDate { get; set; }
+    public string EndDate { get; set; }
+}
