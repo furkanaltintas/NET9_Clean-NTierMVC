6fc5cb316cc77fc2fe56377d8ebe2f7c135856ae
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-10 03:28:57 +0300

    BlogController hazırlandı.

---
+++ Presentation/Areas/Management/Controllers/BlogController.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract.Base;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using Presentation.Areas.Management.Controllers.Base;
+using Presentation.Areas.Management.ViewModels;
+using Presentation.Extensions;
+using Presentation.Helpers;
+
+namespace Presentation.Areas.Management.Controllers;
+
+public class BlogController : BaseController
+{
+    public BlogController(IServiceManager serviceManager, IToastNotification toastNotification) : base(serviceManager, toastNotification) { }
+
+    [Route("")]
+    public async Task<IActionResult> Index()
+    {
+        var result = await _serviceManager.BlogService.GetAllAsync();
+        return this.ResponseViewModel<GetAllBlogDto, BlogViewModel>(result);
+
+        #region Eski Yöntem
+        // Eski yöntem
+        //var viewModels = result.Data.Select(dto => new BlogViewModel(dto)).ToList();
+        //return View(viewModels);
+        #endregion
+    }
+
+    [Route("ekle")]
+    public IActionResult Add() => View();
+
+
+    [HttpPost]
+    public async Task<IActionResult> Add(CreateBlogDto createBlogDto)
+    {
+        var result = await _serviceManager.BlogService.AddAsync(createBlogDto);
+        return this.ResponseRedirectAction(result, _toastNotification);
+    }
+
+
+    [Route("guncelle")]
+    public async Task<IActionResult> Update(int blogId)
+    {
+        var result = await _serviceManager.BlogService.GetAsync(blogId);
+        return this.ResponseView(result);
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Update(UpdateBlogDto updateBlogDto)
+    {
+        var result = await _serviceManager.BlogService.UpdateAsync(updateBlogDto);
+        return this.ResponseRedirectAction(updateBlogDto, ResultHelper.IsSuccess(result), result.Message, _toastNotification);
+    }
+
+    [HttpPost("sil")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var result = await _serviceManager.BlogService.DeleteAsync(id);
+        return this.ResponseRedirectAction(result, _toastNotification);
+    }
+}
---
+++ Presentation/Areas/Management/ViewModels/BlogViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities.Abstract;
+using Entities.Dtos;
+
+namespace Presentation.Areas.Management.ViewModels;
+
+public class BlogViewModel : IViewModel
+{
+    public BlogViewModel(GetAllBlogDto blogDto)
+    {
+        Id = blogDto.Id;
+        Title = blogDto.Title;
+        Slug = blogDto.Slug;
+        Content = blogDto.Content;
+        Image = blogDto.Image;
+        PublishDate = blogDto.PublishDate.ToString("dd/MM/yyyy");
+        IsPublish = blogDto.IsPublish == true ? "Yayında" : "Yayında Değil";
+        IsTrash = blogDto.IsTrash == true ? "Çöp Kutusunda" : "Çöp Kutusunda Değil";
+    }
+
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Slug { get; set; }
+    public string Content { get; set; }
+    public string Image { get; set; }
+    public string PublishDate { get; set; }
+    public string IsPublish { get; set; }
+    public string IsTrash { get; set; }
+    public int LitterBoxTime { get; set; } = 0; // Çöp kutusu süresi
+}
