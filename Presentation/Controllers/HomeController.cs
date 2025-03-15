bf682722e30082d620dbbc480bbbfc82dd3854c7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-15 04:11:59 +0300

    Kod hataları giderildi.

--- Presentation/Controllers/HomeController.cs
+++ Presentation/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using Business.Abstract.Base;
 using Microsoft.AspNetCore.Mvc;
-using NToastNotify;
 using Presentation.Controllers.Base;
 
 namespace Presentation.Controllers;
@@ -8,15 +7,12 @@ namespace Presentation.Controllers;
 [Route("")]
 public class HomeController : ControllerManager
 {
-    private readonly IToastNotification toastNotification;
-    public HomeController(IServiceManager manager, IToastNotification toastNotification) : base(manager)
+    public HomeController(IServiceManager manager) : base(manager)
     {
-        this.toastNotification = toastNotification;
     }
 
     public async Task<IActionResult> Index()
     {
-        toastNotification.AddSuccessToastMessage("Başarılı");
         var result = await _manager.AboutService.GetAboutAsync();
         return View(result.Data);
     }
