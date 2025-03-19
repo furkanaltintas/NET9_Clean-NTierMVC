266e19feba4bdd53b8a7d6e314f136e1f79042bb
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-19 04:04:32 +0300

    Breadcrumb ve Navbar, Layout kısmından ayrıldı

---
+++ Presentation/Areas/Management/ViewComponents/BreadcrumbViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Areas.Management.ViewComponents;
+
+public class BreadcrumbViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ Presentation/Areas/Management/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Areas.Management.ViewComponents
+{
+    public class NavbarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
