124b7f2752253613b8e446ab23b9bc7830d5cfab
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-19 03:58:31 +0300

    Core katmanına taşındı.

---
+++ Core/Aspects/Autofac/Secured/SecuredOperation.cs
@@ -0,0 +1,34 @@
+﻿using Castle.DynamicProxy;
+using Core.Extensions;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Aspects.Autofac.Secured;
+
+public class SecuredOperation : MethodInterception
+{
+    private string[] _roles;
+    private IHttpContextAccessor _httpContextAccessor;
+    public SecuredOperation(string roles)
+    {
+        _roles = roles.Split(',');
+        _httpContextAccessor = ServiceTool.ServiceProvider.GetService<IHttpContextAccessor>();
+    }
+
+    protected override void OnBefore(IInvocation invocation)
+    {
+        List<string> roleClaims = _httpContextAccessor.HttpContext.User.ClaimRoles(); // Kullanıcının rolleri
+
+        foreach (var role in _roles) // Methodların rolleri
+        {
+            if (roleClaims.Contains(role))
+            {
+                return;
+            }
+        }
+
+        throw new Exception("Yetkiniz yok");
+    }
+}
