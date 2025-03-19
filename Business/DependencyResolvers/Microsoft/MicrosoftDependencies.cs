006f158750b0cd43efe0fd216fcda997e2befeed
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-19 03:52:49 +0300

    İş kuralları yapısı oluşturuldu.

--- Business/DependencyResolvers/Microsoft/MicrosoftDependencies.cs
+++ Business/DependencyResolvers/Microsoft/MicrosoftDependencies.cs
@@ -1,7 +1,11 @@
 ﻿using Business.AutoMapper.Profiles;
-using Business.Helpers.Images.Abstract;
-using Business.Helpers.Images.Concrete;
+using Core.Entities.Concrete;
+using Core.Helpers.Images.Abstract;
+using Core.Helpers.Images.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.UnitOfWork;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Business.DependencyResolvers.Microsoft;
 
@@ -9,11 +13,34 @@ public static class MicrosoftDependencies
 {
     public static void AddCustomDependencies(this IServiceCollection services)
     {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
+        services.AddScoped<IRepository, Repository>();
+        services.AddAssemblyServices(assembly);
+
+        services.AddSubClassesOfType(assembly, typeof(BaseBusinessRules));
+
         services.AddScoped<IImageHelper, ImageHelper>();
         services.AddScoped<IFileNameHelper, FileNameHelper>();
 
         services.AddScoped<IImageUploader, ImageUploader>();
 
         services.AddAutoMapper(typeof(AutoMappingProfile).Assembly);
+    }
+
+    public static IServiceCollection AddSubClassesOfType
+    (this IServiceCollection services,
+    Assembly assembly,
+    Type type,
+    Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null)
+    {
+        var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+
+        foreach (var item in types)
+            if (addWithLifeCycle is null)
+                services.AddScoped(item);
+            else
+                addWithLifeCycle(services, type);
+        return services;
     }
 }
---
+++ Business/DependencyResolvers/ServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Business.DependencyResolvers;
+
+public static class ServiceExtensions
+{
+    public static void AddAssemblyServices(this IServiceCollection services, Assembly assembly)
+    {
+        var allTypes = assembly.GetExportedTypes(); // // Derlemeden tüm erişilebilir tipleri bir defa al
+
+        // Hizmet tiplerini filtrelemeye yarar
+        var serviceTypes = allTypes
+            .Where(p => p.IsInterface && !p.IsClass && p.Name.StartsWith("I") && p.Name.EndsWith("Service"))
+            .ToList();
+
+        foreach (var serviceType in serviceTypes)
+        {
+            // İlgili yönetici tipini almaya yarıyor
+            var managerTypeName = GetManagerTypeName(serviceType);
+            var managerType = allTypes.FirstOrDefault(t => t.Name == managerTypeName);
+
+            // Yönetici tipi bulunduysa, servisi ekle
+            if (managerType != null)
+                services.AddScoped(serviceType, managerType);
+        }
+    }
+
+    // Manager tipinin adını elde etme
+    private static string GetManagerTypeName(Type serviceType)
+    {
+        return serviceType.Name
+            .Replace("Service", "Manager")
+            .Substring(1); // "IAuthService" -> "AuthManager"
+    }
+}
