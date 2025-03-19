2e31fe3f2fa5a806c2a32ca4ee3b14195532164d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-19 04:02:35 +0300

    Kullanıcı login ve register işlemleri için dto yapıları oluşturuldu

---
+++ Entities/Dtos/UserDtos/GetUserDto.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Abstract;
+
+namespace Entities.Dtos;
+
+public class GetUserDto : IDto
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Profile { get; set; }
+}
---
+++ Entities/Dtos/UserDtos/GetUserLoginDto.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Abstract;
+
+namespace Entities.Dtos;
+
+public class GetUserLoginDto : IDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public bool IsRememberMe { get; set; }
+}
