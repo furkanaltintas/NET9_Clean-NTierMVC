1c133539c4c0d8fefa8fe4079d85a06a907893e5
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-19 04:00:38 +0300

    BCrypt kullanarak şifreleme yapıldı.

---
+++ Core/Utilities/Hashing/BCryptPasswordService.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Utilities.Hashing;
+
+public static class BCryptPasswordService
+{
+    public static string HashPassword(string password) =>
+        BCrypt.Net.BCrypt.HashPassword(password);
+
+    public static bool VerifyPassword(string inputPassword, string hashedPassword) =>
+        BCrypt.Net.BCrypt.Verify(inputPassword, hashedPassword);
+}
