02ee0087e1686f9dbb9914907c97e8eec199c40a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-19 04:05:54 +0300

    Blog Detail yapısı ViewModel sistemine geçildi

---
+++ Presentation/ViewModels/Blogs/GetBlogViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using Entities.Dtos;
+
+namespace Presentation.ViewModels;
+
+public class GetBlogViewModel : IViewModel
+{
+    public GetBlogDto GetBlogDto { get; set; }
+    public IList<GetAllSocialMediaIconDto> SocialMediaIconDtos { get; set; }
+
+    public GetBlogViewModel(GetBlogDto blog, IList<GetAllSocialMediaIconDto> socialMediaIconDtos)
+    {
+        GetBlogDto = blog;
+        SocialMediaIconDtos = socialMediaIconDtos;
+    }
+}
