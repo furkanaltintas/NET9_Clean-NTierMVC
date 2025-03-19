0de0a817f69f7c259966e8fbbb61eb6375dedb08
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-19 04:03:36 +0300

    Cache silme işlemi yapıldı

---
+++ Presentation/Areas/Management/Filters/NoCacheAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Presentation.Areas.Management.Filters;
+
+public class NoCacheAttribute : Attribute, IActionFilter
+{
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        return;
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        context.HttpContext.Response.Headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0";
+        context.HttpContext.Response.Headers["Pragma"] = "no-cache";
+        context.HttpContext.Response.Headers["Expires"] = "-1";
+
+    }
+}
