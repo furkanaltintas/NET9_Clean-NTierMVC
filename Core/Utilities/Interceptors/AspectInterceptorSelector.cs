4510d4f5f50141b077ea8874b684dbc93359c821
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-25 01:09:35 +0300

    AspectInterceptor yapısı güncellendi. Çoklu işlemlere artık izin veriyor.

--- Core/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -8,9 +8,16 @@ public class AspectInterceptorSelector : IInterceptorSelector
     public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
     {
         var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>(inherit: true).ToList();
-        var methodAttributes = type.GetMethod(method.Name).GetCustomAttributes<MethodInterceptionBaseAttribute>(inherit: true);
 
-        classAttributes.AddRange(methodAttributes ?? Enumerable.Empty<MethodInterceptionBaseAttribute>());
+        //var methodAttributes = type.GetMethod(method.Name).GetCustomAttributes<MethodInterceptionBaseAttribute>(inherit: true);
+        // Eski kod
+
+        var methodAttributes = type.GetMethods()
+            .FirstOrDefault(m => m.Name == method.Name && m.GetParameters().Length == method?.GetParameters().Length)?
+            .GetCustomAttributes<MethodInterceptionBaseAttribute>(inherit: true)
+            ?? Enumerable.Empty<MethodInterceptionBaseAttribute>();
+
+        classAttributes.AddRange(methodAttributes);
 
         return classAttributes.OrderBy(x => x.Priority).ToArray();
     }
