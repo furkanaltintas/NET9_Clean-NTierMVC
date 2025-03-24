f58bcb71383a64e9d9ad22fb197c73761fcd50a7
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-25 01:11:30 +0300

    güncelleme

--- Core/Extensions/ServiceCollectionExtensions.cs
+++ Core/Extensions/ServiceCollectionExtensions.cs
@@ -9,9 +9,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddDependencyResolvers(this IServiceCollection services, ICoreModule[] modules)
     {
         foreach (var module in modules)
-        {
             module.Load(services); // Bütün modülleri ekledik
-        }
 
         return ServiceTool.Create(services);
     }
