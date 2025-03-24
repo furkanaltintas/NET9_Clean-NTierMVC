540f336cf69e853207ddd7c8d646ae830b5aeb2a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-25 01:12:28 +0300

    Oluşturuldu

---
+++ Entities/Dtos/TypeOfEmploymentDtos/CreateTypeOfEmploymentDto.cs
@@ -0,0 +1,8 @@
+﻿using Core.Entities.Abstract;
+
+namespace Entities.Dtos;
+
+public class CreateTypeOfEmploymentDto : IDto
+{
+    public string Name { get; set; }
+}
---
+++ Entities/Dtos/TypeOfEmploymentDtos/GetTypeOfEmploymentDto.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Abstract;
+
+namespace Entities.Dtos;
+
+public class GetTypeOfEmploymentDto : IDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public virtual ICollection<GetAllExperienceDto> GetAllExperienceDtos { get; set; }
+}
