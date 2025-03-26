a9ff0a1a17fefd5bfdaa79c9e15cf82886bd7b35
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-26 04:10:41 +0300

    About kodları hazırlandı

--- Presentation/Areas/Management/Controllers/AboutController.cs
+++ Presentation/Areas/Management/Controllers/AboutController.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Modules.Abouts.Services;
 using Entities.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Portfolio.Core.Utilities.Results.Abstract;
 using Presentation.Areas.Management.Controllers.Base;
 using Presentation.Extensions;
+using IResult = Portfolio.Core.Utilities.Results.Abstract.IResult;
 
 namespace Presentation.Areas.Management.Controllers;
 
@@ -10,21 +12,40 @@ public class AboutController(IAboutService aboutService) : BaseController
 {
     public async Task<IActionResult> Index()
     {
-        var result = await aboutService.GetAboutForUpdateAsync();
+        IDataResult<GetAboutDto> result = await aboutService.GetAboutAsync();
         return this.ResponseView(result);
     }
+
+
+    public IActionResult Add() => View();
+
+
+    [HttpPost]
+    public async Task<IActionResult> Add(CreateAboutDto createAboutDto)
+    {
+        IResult result = await aboutService.CreateAboutAsync(createAboutDto);
+        return this.ResponseRedirectAction(result, ToastNotification, createAboutDto);
+    }
+
+
+    public async Task<IActionResult> Update()
+    {
+        IDataResult<UpdateAboutDto> result = await aboutService.GetAboutForUpdateAsync();
+        return this.ResponseView(result);
+    }
+
 
     [HttpPost]
     public async Task<IActionResult> Update(UpdateAboutDto updateAboutDto)
     {
-        var result = await aboutService.UpdateAboutAsync(updateAboutDto);
+        IResult result = await aboutService.UpdateAboutAsync(updateAboutDto);
         return this.ResponseRedirectAction(result, ToastNotification, updateAboutDto);
     }
 
 
     public async Task<IActionResult> Delete()
     {
-        var result = await aboutService.DeleteAboutAsync();
+        IResult result = await aboutService.DeleteAboutAsync();
         return this.ResponseRedirectAction(result, ToastNotification);
     }
 }
