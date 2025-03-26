4d155ec4994b174647646c1adae473dcff901303
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-26 04:09:53 +0300

    Captcha Koruması ve Nesne içinde ki boş proplara göre durum dönen helper yazıldı.

---
+++ Core/Helpers/CaptchaResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Helpers;
+
+public class CaptchaResult
+{
+    public string CaptchaCode { get; set; }
+    public byte[] CaptchaByteData { get; set; }
+    public string CaptchaBase64Data => Convert.ToBase64String(CaptchaByteData);
+    public DateTime TimeStamp { get; set; }
+}
---
+++ Core/Helpers/Properties.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Helpers;
+
+public static class Properties
+{
+    /// <summary>
+    /// Herhangi bir özelliğin değeri null ise 'isProperty değişkeni' true olur.
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <param name="entity"></param>
+    public static Boolean GetPropertiesIsNull<TEntity>(TEntity entity)
+        where TEntity : class, new()
+    {
+        Boolean isProperty = entity.GetType().GetProperties().Where(p => p.GetValue(entity) == null).Any();
+        return isProperty;
+    }
+}
